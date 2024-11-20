de2173a0a2d0785682fbd23cac0e41dedc896efd
FurkanIpek0 <170216383+FurkanIpek0@users.noreply.github.com>
2024-11-21 01:56:08 +0300

    Use if and loop for error management and sum

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
-};
+const sumAll = function(firstNumber, secondNumber) {
+    if (firstNumber < 0 ||
+        secondNumber < 0 ||
+        firstNumber%1 !== 0 ||
+        secondNumber%1 !== 0 ||
+        typeof(firstNumber) !== 'number' ||
+        typeof(secondNumber) !== 'number') return 'ERROR';
+    if (firstNumber == secondNumber) {
+        return firstNumber;
+    }
+    let bigNumber = firstNumber > secondNumber ? firstNumber : secondNumber;
+    let smallNumber = firstNumber < secondNumber ? firstNumber : secondNumber;
+    let sum = 0;
+    for (;smallNumber <= bigNumber; smallNumber++){
+        sum += smallNumber;
+    }
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
