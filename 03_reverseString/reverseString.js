98f30ac2beff5327eff0f3fad0184dbd4c20bbf0
FurkanIpek0 <170216383+FurkanIpek0@users.noreply.github.com>
2024-11-20 21:13:38 +0300

    Use array and loops for reverse text

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    const textArray = text.split("");
+    let newText = '';
+    for (i = text.length - 1; i >= 0; i--){
+        newText += textArray[i];
+    }
+    return newText;
+    ;
 };
 
 // Do not edit below this line
