23cc088dd1a754898edee0bf91a7c1f811468e5d
FurkanIpek0 <170216383+FurkanIpek0@users.noreply.github.com>
2024-11-21 02:52:16 +0300

    Use 2 functions for converting celcius and fhrnit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitDegree) {
+    let celciusDegree = (fahrenheitDegree - 32) * 5 / 9;
+    celciusDegree = Number.isInteger(celciusDegree) ? celciusDegree : celciusDegree.toFixed(1);
+    return +celciusDegree;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celciusDegree) {
+    let fahrenheitDegree = (9 / 5 * celciusDegree) + 32;
+    fahrenheitDegree = Number.isInteger(fahrenheitDegree) ? fahrenheitDegree : fahrenheitDegree.toFixed(1);
+    return +fahrenheitDegree;
 };
 
 // Do not edit below this line
