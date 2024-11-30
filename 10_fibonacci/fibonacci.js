976f9daf43d3489fb023ac20268087db3b52cf09
FurkanIpek0 <170216383+FurkanIpek0@users.noreply.github.com>
2024-11-30 19:34:15 +0300

    solve problems

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number == 0) {
+        return 0;
+    }
+    if (number < 0) {
+        return 'OOPS';
+    }
+    const fibonacciArray = [];
+    for(i = 0; i < number; i++) {
+        if(fibonacciArray.length < 2) {
+            fibonacciArray.push(1);
+        }
+        else {
+            fibonacciArray.push(fibonacciArray[fibonacciArray.length - 1] + fibonacciArray[fibonacciArray.length - 2]);
+        }
+    }
+    return fibonacciArray[number - 1];
 };
 
 // Do not edit below this line
