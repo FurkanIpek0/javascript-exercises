dbe5be41449b7380f7992d7fe1d73d72819d00bd
FurkanIpek0 <170216383+FurkanIpek0@users.noreply.github.com>
2024-11-30 19:12:01 +0300

    solve problems

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const newStr = str.toLowerCase().replace(/!| |,|\./gi, '');
+    console.log(newStr);
+    return newStr.split('').reverse().join('') == newStr;
 };
 
 // Do not edit below this line
