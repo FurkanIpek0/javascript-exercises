7a24727cbdf73a70f0671c9ba7fbd2fd3b384f1f
FurkanIpek0 <170216383+FurkanIpek0@users.noreply.github.com>
2024-11-30 18:49:03 +0300

    solve problems

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+	return args[0].reduce((sum, currenValue) => currenValue + sum, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	return Math.pow(number, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let factorial = 1;
+	for (i = number; i > 1; i--) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
